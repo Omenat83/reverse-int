5329275e3c07b81a487b91cd55b5da206f53fabd
Maryna K <110280285+Omenat83@users.noreply.github.com>
2024-08-04 16:21:04 +0300

    feat: task ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let result = "";
+    if (n < 0) n = n * -1;
+    let str = "" + n;
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        result += str[i];
+    }
+
+    return Number.parseInt(result);
+};
